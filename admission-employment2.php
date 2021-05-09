5ce57beddff869de3c8f3d2f16f00fecdb826fdf
Abdallah7x <76527450+Abdallah7x@users.noreply.github.com>
2021-05-09 16:49:44 +0200

    Create admission-employment2.php

---
+++ admission-employment2.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="stylesheet.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+<title>Sharm El-Sheikh School - admission</title>
+</head>
+<body>
+  <div class="topnav1" id="myTopnav1">
+
+  <a href="index.html" style="float:left; "> <img src="logo.png" class="logo"></a>
+  <a href="login.php">Portal</a>
+  <a href="admission-employment2.php" id="active">Employment</a>
+  <a href="newstudents2.php">Admission</a>
+  <a href="#news">School Life</a>
+  <a href="#contact">News/Events</a>
+  <a href="about.html">About Us</a>
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+    </a>
+  </div>
+  <img src="bgimage.jpg" class="bgimage">
+  <div class="opacity">
+
+
+
+
+    <div class="divideall">
+
+    <div id="left222">
+
+    <p class="vacancies" style="text-align:center;"><b style="text-align:center;">
+VACANCIES
+    </b></p>
+    </div>
+    <div class="form">
+      <p class="mistext"><b style="color:white;">Employments<br><br>PLease fill the form below</b></p>
+      <form action="admission-employment3.php">
+    <label class="label1" for="fname">First name:</label><br>
+    <input  type="text" id="fname" name="fname" placeholder="Example" required><br>
+    <label class="label1" for="lname">Middle name:</label><br>
+    <input  type="text" id="lname" name="mname" placeholder="Example" required><br>
+    <label class="label1" for="fname">Last name:</label><br>
+    <input " type="text" id="fname" name="lname" placeholder="Example" required><br>
+    <label class="label1" for="lname">Phone number:</label><br>
+    <input " type="text" id="lname" name="phoneNumer" placeholder="Example" required><br>
+    <label class="label1" for="fname">Age:</label><br>
+    <input  type="text" id="fname" name="age" placeholder="Example" required><br>
+    <b><input class="mission12" type="submit" value ="NEXT"></b>
+
+    </div>
+    </div>
+
+
+
+
+
+  </div>
+
+
+
+
+
+
+
+ 
+</body>
+</html> 
