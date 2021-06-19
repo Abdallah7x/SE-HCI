e667f8ee3c10d49213a8f4f28e0377b3a9422096
seifeldinahmed731 <83872282+seifeldinahmed731@users.noreply.github.com>
2021-06-19 04:04:45 +0200

    Update newstudents3.php

--- newstudents3.php
+++ newstudents3.php
@@ -1,5 +1,8 @@
 <?php
-REQUIRE_ONCE "../classes/users.php";
+error_reporting(0);
+include_once("../View/userView.php");
+$Userview = new ViewUsers;
+session_start();
 ?>
 <!DOCTYPE html>
 <html>
@@ -13,48 +16,41 @@ REQUIRE_ONCE "../classes/users.php";
 <title>Sharm El-Sheikh School - admission</title>
 </head>
 <body>
-  <div class="topnav" id="myTopnav">
+ <!--  <div class="topnav" id="myTopnav">
 
   <a href="index.html" style="float:left; "> <img src="logo.png" class="logo"></a>
   <a href="login.php">Portal</a>
   <a href="admission-employment2.php" >Employment</a>
   <a href="newstudents2.php" id="active">Admission</a>
   <a href="#news">School Life</a>
-  <a href="#contact">News/Events</a>
-  <a href="about.html">About Us</a>
+  <a href="about.php">About Us</a>
   <a href="javascript:void(0);" class="icon" onclick="myFunction()">
     <i class="fa fa-bars"></i>
   </a>
-  </div>
+  </div> -->
+  <?php
+  $Userview->navigation();
+  ?>
   <img src="bgimage.jpg" class="bgimage">
   <div class="opacity">
 
-
-
-
     <div class="divideall">
 
-    <div id="left224">
-
-    <p class="admissiontext"><b>
-      Sharm El Sheikh College aims is to provide opportunities for a better future through providing students with the 21st Century skills and quality education that meets the needs of the community. Children come to an understanding that they live in a special unique place, and that they need to have tolerance for all.
-
-    </b></p>
+    <div id="left224" style="margin-top: 100px;">
+        <p class="admissionword">Admission</p>
+    <p class="admissiontext" >
+         <b>Sharm El Sheikh College aims is to provide opportunities for a better future through providing students with the 21st Century skills and quality education that meets the needs of the community. Children come to an understanding that they live in a special unique place, and that they need to have tolerance for all.<br>
+        We at Sharm El Sheikh College find it our duty to nurture today’s children as they are tomorrow’s future. We instil in our children the love of seeking knowledge and the belief that it is an ongoing, never-ending process. We prepare them for the ever-changing world around us, encouraging them to become design thinkers and entrepreneurs within the expanding global community of the 21st Century. We can achieve this by instilling SSC’s Personal Core Values within our daily school life, assisting them to face the unknown and be ready for any unexpected circumstance. We also believe that fulfilling these goals should be a joint venture shared by both the School and the Parents.
+        </b>
+    </p>
     </div>
-
     <div class="form1">
-      <p class="mistext"><b>Admission<br><br>PLease fill the form below</b></p>
-  <form method="POST">
-    <label class="label1" for="fname">Gender:</label><br>
-   
-    <select  class="input1"name ="gender">
-      <option value="M">Male</option>
-      <option value="F">Female</option>
-  </select><br>
-    <label class="label1" for="lname">Email:</label><br>
-    <input class="input1" type="email" id="lname" name="email" placeholder="Example" required><br>
-    <b><input class="mission12" type="submit" value ="SUBMIT"></b>
-</form>
+    <p class="mistext">Please fill the form below</b></p>
+<?php
+
+$Userview->newStudent3($_GET["fname"],$_GET["mname"],$_GET["lname"],$_GET["phoneNumer"],$_GET["age"]);
+
+ ?>
     </div>
 
 
@@ -84,17 +80,17 @@ REQUIRE_ONCE "../classes/users.php";
 </body>
 </html>
 <?php
-if ($_POST){
-$Users = new Users;
-$Users->userFName=$_GET["fname"];
-$Users->userMName=$_GET["mname"];
-$Users->userLName=$_GET["lname"];
-$Users->email=$_POST["email"];
-$Users->phoneNo=$_GET["phoneNumer"];
-$Users->Age=$_GET["age"];
-$Users->Gender=$_POST["gender"];
-$Users->UserType = 2;
-$Users->AddUsers();
-header("Location:index.html");
-}
+// if ($_POST){
+// $Users = new Users;
+// $Users->userFName=$_GET["fname"];
+// $Users->userMName=$_GET["mname"];
+// $Users->userLName=$_GET["lname"];
+// $Users->email=$_POST["email"];
+// $Users->phoneNo=$_GET["phoneNumer"];
+// $Users->Age=$_GET["age"];
+// $Users->Gender=$_POST["gender"];
+// $Users->UserType = 2;
+// $Users->AddUsers();
+// header("Location:index.html");
+// }
 ?>
